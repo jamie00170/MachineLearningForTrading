4d3ff307fc51f8f67fb05680cfcfdee36212d0c1
Jamie Dale <jamiedd13@aol.com>
2016-11-08 15:08:02 +0000

    Refactor and add code to plot

--- join_data_example.py
+++ join_data_example.py
@@ -1,29 +1,63 @@
 '''Build a dataframe in pandas'''
 import pandas as pd
+import os 
+import matplotlib.pyplot as plt
+
+def symbol_to_path(symbol, base_dir='data'):
+	return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+def get_data(symbols, dates):
+	
+	df = pd.DataFrame(index=dates)
+	# use spy as reference 
+	if 'SPY' not in symbols:
+		symbols.insert(0, "SPY")
+
+	for symbol in symbols:
+		df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
+							   parse_dates=True, usecols=['Date', 'Adj Close'],
+							   na_values=['nan'])
+
+		# rename to prevent clash
+		df_temp = df_temp.rename(columns={'Adj Close':symbol})
+
+		df = df.join(df_temp) 
+
+	df = df.dropna()
+	return df
+
+def plot_data(df, title="Stock prices", x_label='Date', y_label="Price"):
+	'''Plot stock prices given a dataframe'''
+	ax = df.plot(title=title, fontsize=2)
+	ax.set_xlabel(x_label)
+	ax.set_ylabel(y_label)
+
+	plt.show() # must be called to show plot
+
 
 def main():
 	# Define date range
-	start_date = '2010-01-22'
-	end_date = '2010-01-26'
+	start_date = '2010-01-01'
+	end_date = '2010-12-31'
 	
 	# create an array of datetime index objects for each date in range
 	dates = pd.date_range(start_date, end_date)
 	
-	# Create an empty dataframe
-	df1 = pd.DataFrame(index=dates)
+	symbols = ["SPY", "IBM", "GLD", "AAPL"]
 
-	# Read SPY data into temporary dataframe, using date as the index column
-	# which allows a join with our created dataframe
-	dfSPY = pd.read_csv("data/SPY.csv", index_col="Date", parse_dates=True,
-						usecols=['Date', 'Adj Close'], na_values=['nan'])
-	
-	# Join the two dataframes, uses a left join (only retain rows that are in df1)
-	df1 = df1.join(dfSPY)
+	df = get_data(symbols, dates)
 
-	# Drop NaN values i.e weekend dates
-	df1 = df1.dropna()
+	# Slice by row
+	#print df.ix['2010-01-01': '2010-01-31']
 
-	print df1
+	# Slice by column
+	#print df['AAPL']
+	#print df[['IBM', 'GLD']]
+
+	# Slice by row and column
+	#print df.ix['2010-01-01': '2010-01-31', ['AAPL', 'IBM']]
+
+	plot_data(df)
 
 if __name__ == "__main__":
 	main()
